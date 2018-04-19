b4080df54d3fe11ab38901bb018fce7c7c8ba096
Brian Henry <brian@>
2018-04-19 09:29:43 -0700

    Fixing a few errors

--- Controllers/CDController.cs
+++ Controllers/CDController.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CDOrganizer.Controllers;
 
-namespace CDController
+namespace CDController{
     public class CDController : Controller
     {
 
