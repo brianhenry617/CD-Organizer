31bde06b365283ed702c1132b3545a93d9c951dd
Brian Henry <brian@>
2018-04-19 08:44:00 -0700

    Add Controller for CDs

---
+++ Controllers/CDController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CD-Organizer.Models;
+
+namespace CDController.Controllers
+{
+    public class CDController : Controller
+    {
+
+        [HttpGet("/CDs-list")]
+        public ActionResult Index()
+        {
+          List<CDs> allCDs = CDs.GetAll();
+          return View(allCDs);
+        }
+
+        [HttpGet("/CDs-list/new")]
+        public ActionResult CreateForm()
+        {
+          return View();
+        }
+
+        [HttpPost("/CDs-list")]
+        public ActionResult Create()
+        {
+          CDs newCDs = new CDs (Request.Form["new-CDs"]);
+          newCDs.Save();
+          List<CDs> allCDs = CDs.GetAll();
+          return View("Index", allCDs);
+        }
+
+        [HttpPost("/CDs/delete")]
+        public ActionResult DeleteAll()
+        {
+            Place.ClearAll();
+            return View();
+        }
+    }
+}
